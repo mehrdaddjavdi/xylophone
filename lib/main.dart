31326eb5c2aacc3047c2ebb8f45bc7e0d428ddbd
mehrdaddjavadi <mehrdaddjavadi@gmail.com>
2023-03-02 14:27:19 -0800

    project updated

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,81 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioPlayer();
-                player.play(AssetSource('note1.wav'));
-              },
-              child: Text('Fuck You'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+                  color: Colors.red,
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note1.wav'));
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Color.fromARGB(255, 54, 244, 206),
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note2.wav'));
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Color.fromARGB(255, 16, 238, 71),
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note3.wav'));
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Color.fromARGB(255, 156, 4, 212),
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note4.wav'));
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Color.fromARGB(255, 245, 190, 10),
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note5.wav'));
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Color.fromARGB(255, 4, 0, 10),
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note6.wav'));
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Color.fromARGB(212, 0, 247, 136),
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note7.wav'));
+                  },
+                  child: Text(''),
+                ),
+              ),
+            ],
           ),
         ),
       ),
